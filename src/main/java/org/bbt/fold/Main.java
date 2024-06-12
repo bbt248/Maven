35d473109d33d1ecef312ab08c8b9853b5fb6633
PersonList <hewlett.packard.windows11@gmail.com>
2024-06-12 17:40:59 +0300

    u

---
+++ src/main/java/org/bbt/fold/Main.java
@@ -0,0 +1,17 @@
+package org.bbt.fold;
+import org.junit.jupiter.api.Test;
+
+public class Main {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World! [test]");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+
+    }
+}
